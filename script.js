e82c1a2d6f403febf70b17ab77b80be246e7c70f
emahWD <147956248+emahWD@users.noreply.github.com>
2023-10-28 01:54:44 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// Function to toggle the mobile menu
+function toggleMenu() {
+    var navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('active');
+}
+
+// Add an event listener to the menu button
+var menuButton = document.querySelector('.menu-button');
+menuButton.addEventListener('click', toggleMenu);
+
+
+// Optional JavaScript for hiding the preloader after a certain time (e.g., 3 seconds).
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+      const preloader = document.querySelector(".preloader");
+      preloader.style.display = "none";
+    }, 3000); // Hide the preloader after 3 seconds
+  });
+  
